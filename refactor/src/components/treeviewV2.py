73f7a708c48925a1995d488a9341857724e0454a
David Gutman <dagutman@gmail.com>
2023-07-18 20:50:32 -0400

    removing code to change icon to close and open

--- refactor/src/components/treeviewV2.py
+++ refactor/src/components/treeviewV2.py
@@ -24,16 +24,19 @@ app = dash.Dash(
 root_folder = "/home/dagutman/devel"
 
 
+## There's folder  and then folder-close and folder-open
+
+
 def folder_div(folder_path, level=0):
     return html.Div(
         [
             dmc.Button(
                 os.path.basename(folder_path),
-                rightIcon=DashIconify(icon="carbon:settings-check", width=20),
+                # rightIcon=DashIconify(icon="akar-icons:folder-open", width=20),
                 leftIcon=DashIconify(
-                    icon="akar-icons:folder-close"
+                    icon="material-symbols:folder-close"
                     if level % 2 == 0
-                    else "akar-icons:folder-open"
+                    else "material-symbols:folder-open"
                 ),
                 id={"type": "folder", "path": folder_path},
                 n_clicks=0,
@@ -71,6 +74,7 @@ app.layout = html.Div(
     [State({"type": "folder", "path": MATCH}, "id")],
 )
 def display_selected_folder(n_clicks, folder_id):
+    print(folder_id)
     if n_clicks > 0:
         return f"You selected folder: {folder_id['path']}"
     return "No folder selected"
@@ -86,6 +90,10 @@ def toggle_folder(n_clicks, folder_id):
     if n_clicks % 2 == 1:  # folder was expanded
         try:
             subfolders = os.listdir(folder_id["path"])
+            if subfolders:  # if there are subfolders, update the folder icon to "open"
+                folder_id["icon"] = "material-symbols:folder-open"
+            else:  # if there are no subfolders, keep the folder icon as "closed"
+                folder_id["icon"] = "material-symbols:folder-close"
             return [
                 folder_div(os.path.join(folder_id["path"], subfolder), level=level + 1)
                 for subfolder in subfolders
@@ -94,7 +102,29 @@ def toggle_folder(n_clicks, folder_id):
         except FileNotFoundError:
             return []
     else:  # folder was collapsed
+        folder_id["icon"] = "material-symbols:folder-close"
         return []
+
+
+# @app.callback(
+#     Output({"type": "subfolders", "path": MATCH}, "children"),
+#     [Input({"type": "folder", "path": MATCH}, "n_clicks")],
+#     [State({"type": "folder", "path": MATCH}, "id")],
+# )
+# def toggle_folder(n_clicks, folder_id):
+#     level = folder_id["path"].count(os.sep) - root_folder.count(os.sep)
+#     if n_clicks % 2 == 1:  # folder was expanded
+#         try:
+#             subfolders = os.listdir(folder_id["path"])
+#             return [
+#                 folder_div(os.path.join(folder_id["path"], subfolder), level=level + 1)
+#                 for subfolder in subfolders
+#                 if os.path.isdir(os.path.join(folder_id["path"], subfolder))
+#             ]
+#         except FileNotFoundError:
+#             return []
+#     else:  # folder was collapsed
+#         return []
 
 
 if __name__ == "__main__":
