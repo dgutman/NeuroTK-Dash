89be7aae238096d1c1b7b39c574685ef364cf401
David Gutman <dagutman@gmail.com>
2023-08-07 13:25:43 -0400

    working on cacheing annotations

--- refactor/src/utils/database.py
+++ refactor/src/utils/database.py
@@ -1,11 +1,37 @@
 import pandas as pd
 from flask_mongoengine import MongoEngine
-from .settings import MONGO_URI
+from .settings import MONGO_URI, MONGODB_DB
 from flask import Flask
+import pymongo
+
 
 # initialize the app with the extension
 
 db = MongoEngine()
+
+
+## Since the annotations do not have a very rigid schema and have weird fields, not sure if I want a class or not
+
+mc = pymongo.MongoClient(MONGO_URI)
+mc = mc[MONGODB_DB]  ### Attach the mongo client object to the database I want to store everything
+
+
+
+def insertAnnotationData( annotationItems, projectName):
+    ### This will insert all of the annotations pulled from the DSA and also insert a projectName to keep things bundled/separate
+    ## Add the projectName to all of the annotations as well
+    annotationItems = [dict(item, **{'projectName':projectName}) for item in annotationItems]
+    ### The collection for annotations is called.. annotations!
+    print(len(annotationItems))
+    mc['annotationData'].insert_many(annotationItems,upsert=True)
+    return len(annotationItems)
+
+
+def getAnnotationData_fromDB( projectName, filters=None):
+    ## Can add additional filters to mongo query in the future other than projctName
+    annotationData = list(mc['annotationData'].find({}))
+    return annotationData
+
 
 
 class Records(db.Document):
