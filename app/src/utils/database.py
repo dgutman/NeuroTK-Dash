4ff999f20e6e72956e28cb49371480e8309d3e0e
David Gutman <dagutman@gmail.com>
2023-08-10 13:58:59 -0400

    creating stub for main NeuroTK App

---
+++ app/src/utils/database.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from flask_mongoengine import MongoEngine
+from .settings import MONGO_URI
+from flask import Flask
+
+# initialize the app with the extension
+
+db = MongoEngine()
+
+
+class Records(db.Document):
+    _id = db.StringField()
+    name = db.StringField()
+    blockID = db.StringField()
+    caseID = db.StringField()
+    regionName = db.StringField()
+    stainID = db.StringField()
+
+    def to_dict(self):
+        return {
+            "_id": self._id,
+            "name": self.name,
+            "blockID": self.blockID,
+            "caseID": self.caseID,
+            "regionName": self.regionName,
+            "stainID": self.stainID,
+        }
+
+
+def insert_records(records):
+    Records.objects().delete()
+    for record in records:
+        _id = record["_id"]
+        name = record["name"]
+        blockID = str(record["blockID"]) if not pd.isna(record["blockID"]) else None
+        caseID = str(record["caseID"]) if not pd.isna(record["caseID"]) else None
+        regionName = str(record["regionName"]) if not pd.isna(record["regionName"]) else None
+        stainID = str(record["stainID"]) if not pd.isna(record["stainID"]) else None
+
+        record_obj = Records(
+            _id=_id,
+            name=name,
+            blockID=blockID,
+            caseID=caseID,
+            regionName=regionName,
+            stainID=stainID,
+        )
+        record_obj.save()
+    print("Records inserted successfully")
+
+
+def get_all_records_df():
+    records = Records.objects.all()
+    records = [record.to_dict() for record in records]
+    df = pd.DataFrame(records)
+    return df
