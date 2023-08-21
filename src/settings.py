699a2a1597ff47eb3c3706cad9ff92f953d72ad5
jc <jvizcar@emory.edu>
2023-08-21 12:55:13 -0400

    setting updated

--- src/settings.py
+++ src/settings.py
@@ -1,27 +1,15 @@
-# DSA settings.
+# Global variables.
 import os, girder_client
 
-# DSA variables.
-DSA_BASE_URL = "https://megabrain.neurology.emory.edu/api/v1"
+# URL to the API of the DSA instance being used.
+DSA_API_URL = "https://megabrain.neurology.emory.edu/api/v1"
 
-# DSA_API_KEY = os.getenv("DSAKEY")
+# Authenticate girder client.
+gc = girder_client.GirderClient(apiUrl=DSA_API_URL)
 
-gc = girder_client.GirderClient(apiUrl=DSA_BASE_URL)
+### DEVEL ###
+DSA_API_KEY = os.getenv("DSAKEY")
 
-# JC API Key
-gc.authenticate(apiKey="")
-
-# Neurotk API Key
-# gc.authenticate(apiKey='')
-
-# Hard code the user for now.
+gc.authenticate(apiKey=DSA_API_KEY)
 USER = "jvizcar"
-
-# ID to the Projects folder in the NeuroTK collection.
 PROJECTS_FLD_ID = "64dbd2667920606b462e5b83"
-
-# SAMPLE_PROJECT_FOLDER = "64dbd2a87920606b462e5b85"
-
-# if DSA_API_KEY:
-#     _ = gc.authenticate(apiKey=DSA_API_KEY)
-#     print(_)
