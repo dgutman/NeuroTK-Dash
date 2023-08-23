95e2ee95c2f8fde9ead47a809bad211757878591
jc <jvizcar@emory.edu>
2023-08-23 17:02:53 -0400

    removing imports not used

--- src/components/projects_frame.py
+++ src/components/projects_frame.py
@@ -4,11 +4,7 @@ This file contains the HTML view loaded when the "Projects" tab is selected.
 Components include project and task selection (dropdowns), datatable and 
 dataview components, and buttons to add and delete new projects and tasks.
 """
-from dash import html, Output, Input, callback, dcc
-import dash_mantine_components as dmc
-import dash_bootstrap_components as dbc
-import numpy as np
-import pandas as pd
+from dash import html
 
 from .project_selection import project_selection
 from .task_selection import task_selection
@@ -20,4 +16,3 @@ projects_frame = html.Div([
     task_selection,
     dataset_view
 ], id="projects-frame")
-
