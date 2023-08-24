049443b74fc32e1e8dc03c4a392960635ecbecb9
David Gutman <dagutman@gmail.com>
2023-08-24 12:25:43 -0400

    updating dataview with pagination

--- src/components/dataset_view.py
+++ src/components/dataset_view.py
@@ -7,40 +7,66 @@ import pandas as pd
 from ..utils.database import getProjectDataset
 from ..utils.helpers import generate_generic_DataTable
 from .imageSetViewer import generate_imageSetViewer_layout
+from .dataView_component import generateDataViewLayout
 
-dataset_view = html.Div([
-    dcc.Store("projectItem_store"),
-    dmc.Tabs([
-        dmc.TabsList([
-            dmc.Tab("Table", value="table"),
-            dmc.Tab("Images", value="images"),
-        ]),
-        dmc.TabsPanel([
-            html.Div(
-                html.Div(),
-                id="project-itemSet-div",
-            ),
-        ], value="table", id="table_tab_panel"),
-        dmc.TabsPanel([
-            html.Div(
-                html.Div(),
-                id="images_div",
-            ),
-        ], value="images", id="images_tab_panel")
-    ], orientation="vertical", value="table")
-])
+dataset_view = html.Div(
+    [
+        dcc.Store("projectItem_store"),
+        dcc.Store("itemSet-state"),  # Storing itemSet as state
+        dmc.Tabs(
+            [
+                dmc.TabsList(
+                    [
+                        dmc.Tab("Table", value="table"),
+                        dmc.Tab("Images", value="images"),
+                    ]
+                ),
+                dmc.TabsPanel(
+                    [
+                        html.Div(
+                            html.Div(),
+                            id="project-itemSet-div",
+                        ),
+                    ],
+                    value="table",
+                    id="table_tab_panel",
+                ),
+                dmc.TabsPanel(
+                    [
+                        html.Div(
+                            id="images_div",
+                            children=[
+                                dbc.Pagination(
+                                    id="pagination",
+                                    size="sm",
+                                    active_page=1,
+                                    max_value=1,
+                                ),
+                                html.Div(id="cards-container"),
+                                dbc.RadioItems(
+                                    id="size-selector",
+                                    value="small",  # Default value
+                                    inline=True,
+                                ),
+                            ],
+                        ),
+                    ],
+                    value="images",
+                    id="images_tab_panel",
+                ),
+            ],
+            orientation="vertical",
+            value="images",
+        ),
+    ]
+)
 
 
 @callback(
     Output("projectItem_store", "data"),
-    [
-        Input("projects-dropdown", "value"),
-        Input("projects-dropdown", "data")
-    ]
+    [Input("projects-dropdown", "value"), Input("projects-dropdown", "data")],
 )
-def updateProjectItemStore(
-    projectId: str, projectData: List[dict]
-) -> List[dict]:
+def updateProjectItemStore(projectId: str, projectData: List[dict]) -> List[dict]:
     """
     Updates the projects item store / mongo database when dropdown changes.
 
@@ -50,7 +76,7 @@ def updateProjectItemStore(
 
     Returns:
         List of dictionaries with item / image metadata.
-    
+
     """
     # Get the name of the project.
     projectName = None
@@ -67,14 +93,11 @@ def updateProjectItemStore(
         return projectItemSet
     else:
         return []
-    
+
 
 @callback(
     Output("project-itemSet-div", "children"),
-    [
-        Input("projectItem_store", "data"),
-        Input("tasks-dropdown", "value")
-    ]
+    [Input("projectItem_store", "data"), Input("tasks-dropdown", "value")],
 )
 def updateProjectItemSetTable(
     projectItemSet: List[dict], selectedTask: str
@@ -86,10 +109,10 @@ def updateProjectItemSetTable(
     Args:
         projectItemSet: Projet item information which is a list of dictionaries.
         selectedTask: Value of the task dropdown.
-    
+
     Returns:
         Datatable HTML div.
-    
+
     """
     # If there are items read them into dataframe.
     if projectItemSet:
@@ -100,7 +123,7 @@ def updateProjectItemSetTable(
             taskColName = f"taskAssigned_{selectedTask}"
 
             if taskColName in df:
-                df = df[df[taskColName] == 'Assigned']
+                df = df[df[taskColName] == "Assigned"]
             else:
                 df = pd.DataFrame()
 
@@ -108,7 +131,7 @@ def updateProjectItemSetTable(
         cols_to_drop = []
 
         for col in df.columns.tolist():
-            if col.startswith('taskAssigned_'):
+            if col.startswith("taskAssigned_"):
                 cols_to_drop.append(col)
 
         df = df.drop(columns=cols_to_drop)
@@ -125,6 +148,6 @@ def updateProjectItemSetTable(
 def updateDataView(projectItemSet):
     ## Update view
     if projectItemSet:
-        imageDataView_panel = [generate_imageSetViewer_layout(projectItemSet)]
+        imageDataView_panel = generateDataViewLayout(projectItemSet)
         return imageDataView_panel
     return html.Div()
