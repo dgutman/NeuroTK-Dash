abf5d58061607c0ad5525457f16597571c2a26d0
David Gutman <dagutman@gmail.com>
2023-09-05 10:41:43 -0400

    adding in ability to select mask

--- src/components/app_tabs.py
+++ src/components/app_tabs.py
@@ -43,7 +43,7 @@ app_tabs = html.Div(
                 dmc.TabsPanel([annotations_frame], value="annotations"),
             ],
             orientation="horizontal",
-            value="annotations",
+            value="analysis",
             id="projects-tabs",
         )
     ]
