fe0bd743931e6f179a6a4751e57863a327e5bf96
JC Vizcarra <jvizcar@emory.edu>
2023-10-05 10:18:52 -0400

    updated iframe app to styx

--- iframe-app-example/app.py
+++ iframe-app-example/app.py
@@ -2,12 +2,11 @@
 # embedded iframe of HistomicsUI. Mainly looking to deal with the access token
 # so we can view private images without being logged out everytime we switch
 # images.
-import dash
-from dash import html, Dash, callback, Output, Input
 from dash_mantine_components import Select
+from dash import html, Dash, callback, Output, Input
 
-DSA_URL = "https://megabrain.neurology.emory.edu"
-API_KEY = "pSXuuP3qhWOW5ZqoOLhQMYuDRj9HNPD0FD9qyGm0"
+DSA_URL = "https://styx.neurology.emory.edu"
+API_KEY = "F24SKHTasyuZI1TihEjQU8peB4mzwBYl0tZOTHy6"
 
 app = Dash(__name__)
 
@@ -18,13 +17,13 @@ app.layout = html.Div(
                 label="Select WSI:",
                 placeholder="",
                 id="wsi-select",
-                value="651ebc456b4fa9ed76d42a3e",
+                value="651ec46e8691f6a5f529c9ff",
                 data=[
-                    {"label": "E20-106_1 Tau.svs", "value": "651ebc456b4fa9ed76d42a3e"},
-                    {"label": "E20-106_14 HE.svs", "value": "651ebc456b4fa9ed76d42a3c"},
+                    {"label": "E20-106_1 Tau.svs", "value": "651ec46e8691f6a5f529c9ff"},
+                    {"label": "E20-106_14 HE.svs", "value": "651ec48b8691f6a5f529ca04"},
                     {
                         "label": "E20-106_15 Syn.svs",
-                        "value": "651ebc456b4fa9ed76d42a3a",
+                        "value": "651ec4fa8691f6a5f529ca09",
                     },
                 ],
             )
@@ -36,7 +35,7 @@ app.layout = html.Div(
 
 @callback(Output("iframe", "children"), Input("wsi-select", "value"))
 def return_iframe(img_id):
-    return html.Div(img_id)
+    return html.Iframe(src=DSA_URL)
 
 
 if __name__ == "__main__":
